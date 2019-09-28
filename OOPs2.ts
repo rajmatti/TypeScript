8f8519f8446615f131078ac7273c79174bb50683
Rajgs <36260412+rajmatti@users.noreply.github.com>
2019-09-28 17:23:36 +0530

    Create OOPs2.ts

---
+++ OOPs2.ts
@@ -0,0 +1,15 @@
+class Dog
+{
+    age: number
+    breed: string
+    constructor(age: number, breed: string)
+    {
+        this.age = age
+        this.breed = string
+    }
+    getRelativeAge(): number
+    {
+        return this.age * 7
+    }
+}
+let Spot = new Dog(2, 'Labrador')
